1b2cee4828703c0b9a041b09265b9c7a376dc075
diazware12 <diazwalker98@gmail.com>
2019-10-19 21:27:08 +0700

    first init

---
+++ JumpingOnTheCloud.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int input;
+	int numberArray;
+	int array [200] = {};
+	int start = 0;
+	int curr = 0;
+	int nilai = 0;
+	
+	cin >> input;
+	for (int i = 0 ; i < input; i++)
+	{	
+		cin >> array [i];
+	}
+	
+	for (int i = 0 ; i < input ; i++)
+	{
+		if (start > input-2)
+		{
+			break;
+		}
+		else
+		{
+			
+			if (array[start+2] == 0)
+			{
+				nilai++;
+				start += 2;
+			}
+			
+			else if (array [start+2] == 1)
+			{
+				nilai++;
+				start += 1;
+			}
+
+		}
+	}
+	
+	cout << nilai;
+
+
+
+	return 0;
+}
+
